68aa35ce45169813bea49d38389d89be46ad6704
Adrian Fish <adrian.r.fish@gmail.com>
2022-06-21 13:02:55 +0100

    test_sse

--- test_sse.py
+++ test_sse.py
@@ -2,14 +2,23 @@ import pprint
 import requests
 import sseclient
 import json
+import threading
 
-s = requests.Session()
+def connect(username, password):
 
-r = s.get('http://127.0.0.1/api/login?username=user1&password=user1')
+    s = requests.Session()
 
-url = 'http://127.0.0.1/api/users/me/events'
-headers = {'Accept': 'text/event-stream'}
-r = s.get(url, stream=True, headers=headers)
-client = sseclient.SSEClient(r)
-for event in client.events():
-    pprint.pprint(json.loads(event.data))
+    r = s.get(f'http://127.0.0.1/api/login?username={username}&password={password}')
+
+    url = 'http://127.0.0.1/api/users/me/events'
+    headers = {'Accept': 'text/event-stream'}
+    r = s.get(url, stream=True, headers=headers)
+    client = sseclient.SSEClient(r)
+    for event in client.events():
+        pprint.pprint(json.loads(event.data))
+
+users = ['user1', 'user2', 'user3']
+
+for user in users:
+    x = threading.Thread(target=connect, args=(user, user))
+    x.start()
